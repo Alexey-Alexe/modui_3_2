2a19900a108309a310aa827218c5109007803beb
Professional <aav_2209@mail.ru>
2025-01-14 18:09:46 +0500

    Добавление

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+my_string = ("Perfomans, Alex")
+
+print(my_string)
+print(len(my_string))
+st = "my_string"
+print(st.upper())
+print(st.lower())
+my_string = my_string.replace(" ", "")
+print(my_string)
+my_string = ("my_string")
+print(my_string[0])
+print(my_string[-1])
